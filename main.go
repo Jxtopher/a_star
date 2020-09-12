e44891a5f40dc2fa1ec95bff6619b670de7bff44
david-sk <david.desmeurs@slagkryssaren.com>
2020-09-12 23:42:56 +0200

    Another example.

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 func main() {
-	var xSize uint64 = 4
-	var ySize uint64 = 4
+	var xSize uint64 = 10
+	var ySize uint64 = 10
 	world := make([][]uint8, xSize)
 	for i := range world {
 		world[i] = make([]uint8, ySize)
@@ -17,16 +17,22 @@ func main() {
 
 	w := worldgen.World{Xsize: xSize, Ysize: ySize, Ground: world}
 	w.Ground = [][]uint8{
-		{0, 0, 0, 0},
-		{0, 1, 1, 0},
-		{0, 1, 1, 0},
-		{0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 1, 1, 1, 1, 1, 1, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 1, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 0, 0, 0, 1, 1, 1, 1, 1, 0},
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{1, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
 	}
 
 	var path []worldgen.Coordinate
-	if w.Ground[2][0] == worldgen.Empty && w.Ground[2][3] == worldgen.Empty {
+	if w.Ground[0][0] == worldgen.Empty && w.Ground[7][7] == worldgen.Empty {
 		path = astar.Run(
-			w, worldgen.Coordinate{X: 2, Y: 0}, worldgen.Coordinate{X: 2, Y: 3},
+			w, worldgen.Coordinate{X: 0, Y: 0}, worldgen.Coordinate{X: 7, Y: 7},
 		)
 	}
 	fmt.Print(path)
